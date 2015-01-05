5e29476934f5d6721ab6c85272ca4f2ad7ac3430
Daniel E. Renfer <duck@kronkltd.net>
2015-01-04 19:08:39 -0500

    set up as ciste module

--- project.clj
+++ project.clj
@@ -1,6 +1,15 @@
 (defproject jiksnu-atom "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Atom Module for Jiksnu"
+  :url "https://github.com/duck1123/jiksnu-atom"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [
+
+                 [net.kronkltd/jiksnu-core "0.1.0-SNAPSHOT"]
+                 [org.clojure/clojure "1.6.0"]
+                 ;; [org.slf4j/slf4j-api "1.7.9"]
+                 ;; [org.slf4j/slf4j-log4j12 "1.7.9"]
+
+                 ]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]}}
+  )
